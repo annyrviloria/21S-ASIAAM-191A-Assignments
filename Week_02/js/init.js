0bded9663f281c2db8c7f680b12d29490e0e6e16
annyrviloria <annyrviloria@g.ucla.edu>
2021-04-08 16:00:01 -0700

    bug fixed

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -1,5 +1,5 @@
 // JavaScript const variable declaration
-const map = L.map('map').setView([34.0709, -118.444], 15);
+const map = L.map('map').setView([34.024540, -118.426460], 15);
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,3 +10,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let marker = L.marker([34.024540, -118.426460]).addTo(map)
 		.bindPopup('Mi casa<br> Alex lives here too ')
 		.openPopup();
+
+fetch("js/lab1.geojson")
+    .then(response => {
+        return response.json();
+    })
+    .then(data =>{
+        // the Leaflet method to add GeoJSON data
+        L.geoJSON(data, {
+            style: function (feature) {
+                return {color: 'red'};
+            }
+        }).bindPopup(function (layer) {
+            return layer.feature.properties.name;
+        }
+            ).addTo(map)
+    });
--- Week_02/readme.md
+++ Week_02/readme.md
@@ -1,1 +1,3 @@
 # Week 2 Assignment
+https://annyrviloria.github.io/21S-ASIAAM-191A-Assignments/
+https://annyrviloria.github.io/21S-ASIAAM-191A-Assignments/Week_02/index.html
