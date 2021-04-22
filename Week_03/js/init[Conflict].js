29da1d7acf1bd0005b6de1dbb97fbcd6b85d4096
annyrviloria <annyrviloria@g.ucla.edu>
2021-04-22 11:08:37 -0700

    map finalized

--- Week_03/js/init[Conflict].js
+++ Week_03/js/init[Conflict].js
@@ -12,12 +12,12 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
    // return message
 //}
 
-var myIcon = L.icon({iconUrl:'houseIcon.png', iconSize: [50,50]});
+var myIcon = L.icon({iconUrl: 'https://pngtree.com/freepng/vector-house-icon_4013710.png', iconSize: [40,40]});
 
 //new markers function:
 function addMarker(lat,lng,title,message){
     console.log(message)
-    L.marker([lat,lng],{icon:myIcon}).addTo(map).bindPopup(`<h2>${title}</h2>`+`<p>${message}</p>`);
+    L.marker([lat,lng],{icon:myIcon}).addTo(map).bindPopup(`<h2>${title}</h2>`);
     createButtons(lat,lng,title);
     return message
 }
@@ -34,6 +34,6 @@ function createButtons(lat,lng,title){
     document.body.appendChild(newButton); //this adds the button to our page.
 }
 //add markers to map
-addMarker(6.040910, -72.636830,'Socota: My Paternal Homeland','My grandparents come from this small village tucked in the highlights of the Andes. This region is home to the Muisca, which is the indigenous group I trace my ancestry back to. My grandparents come from farming families, and they met and married here in the year 1962')
-addMarker(9.300210, -75.395600, 'Sincelejo: My Maternal Homeland', 'My grandparents on this side of my family come from this coastal town in the Caribbean. My grandma comes from a family of cooks and farmers, which I think is why food plays such an important role in my life. My grandpa came from a family of fishermen. My mom was their 4th child, and this is where she grew up, and where she and my dad met!')
-addMarker(4.629200, -74.144920, 'Bogota: My Homeland','After my mom and dad met and fell in love, they moved to the city together. At this point my dads parents had also moved to the city. This is where my and my 3 brothers were born and raised, and where I go back to when I get a chance to visit home over the holidays.')
+addMarker(6.040910, -72.636830,'Socota: My Paternal Homeland')
+addMarker(9.300210, -75.395600, 'Sincelejo: My Maternal Homeland')
+addMarker(4.629200, -74.144920, 'Bogota: My Homeland')
