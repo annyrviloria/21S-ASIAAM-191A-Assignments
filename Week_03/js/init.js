a2d60037565a04e7e116273e6c4dbe3154e93c7f
annyrviloria <annyrviloria@g.ucla.edu>
2021-04-22 11:21:15 -0700

    map again

--- Week_03/js/init.js
+++ Week_03/js/init.js
@@ -3,7 +3,6 @@ let MapZoom= 10;
 const map = L.map('map').setView(MapCenter, MapZoom);
 
 L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 //function addMarker(lat,lng, message){
