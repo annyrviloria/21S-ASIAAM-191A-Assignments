c63038336cecc1ab4de3a313fea28a824b1e4325
annyrviloria <annyrviloria@g.ucla.edu>
2021-04-30 15:47:16 -0700

    updated with week 5 lab instructions

---
+++ Week_04/js/init.js
@@ -0,0 +1,20 @@
+const map = L.map('map').setView([34.0709, -118.444], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let url = "GIVE_ME_A_URL"
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+        console.log(data)
+    }
+function addMarker(lat,lng,message){
+        L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${message}</h2>`)
+        return message    
+}
+
+addMarker(37,-122,'home land!')
