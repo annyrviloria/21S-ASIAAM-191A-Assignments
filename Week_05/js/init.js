7c0c0a9406881a301613f52b1bbca479d3d73e62
annyrviloria <annyrviloria@g.ucla.edu>
2021-05-01 15:38:09 -0700

    final form

---
+++ Week_05/js/init.js
@@ -0,0 +1,44 @@
+const map = L.map('map').setView([34.0709, -118.444], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function addMarker(data){
+    // console.log(data)
+    // these are the names of our fields in the google sheets:
+    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.timestamp}</h2>`)
+    return data.timestamp
+}
+
+let url = "https://spreadsheets.google.com/feeds/list/1gB_pIq1Y0WGMzJjC8qPA5oGT6sDAfkhZLL_ag1GL3j8/oiz5byp/public/values?alt=json"
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+        //console.log(data)
+        formatData(data)
+    })
+
+
+function formatData(theData){
+    const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */
+    const rows = theData.feed.entry // this is the weird Google Sheet API format we will be removing
+    // we start a for..of.. loop here 
+    for(const row of rows) { 
+      const formattedRow = {}
+      for(const key in row) {
+        // time to get rid of the weird gsx$ format...
+        if(key.startsWith("gsx$")) {
+              formattedRow[key.replace("gsx$", "")] = row[key].$t
+        }
+      }
+      // add the clean data
+      formattedData.push(formattedRow)
+    }
+    // lets see what the data looks like when its clean!
+    console.log(formattedData)
+    // we can actually add functions here too
+    formattedData.forEach(addMarker);
+}
